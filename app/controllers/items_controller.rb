77ba3fb618145585b7ce31dc34b535e2c85439e5
yudaiishiyama <yude3939@gmail.com>
2022-05-25 11:15:08 +0900

    商品出品から保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,24 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+  def index
+    @item = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :price, :description, :category_id, :status_id, :state_id,:fee_payer_id,:delivery_time_id,).merge(user_id: current_user.id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,6 @@ class User < ApplicationRecord
   validates :family_name_kana, presence: true,format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
   validates :first_name_kana, presence: true,format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
   validates :birthday, presence: true
+
+  has_many :items
 end
