dbcf046a202c0e14c1448e715bfd08752399d410
yudaiishiyama <yude3939@gmail.com>
2022-05-25 14:49:47 +0900

    修正2

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.last_name }
     email { Faker::Internet.free_email }
-    password { Faker::Internet.password(min_length: 6) }
+    password { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     first_name { '田中' }
     family_name { '太郎' }
