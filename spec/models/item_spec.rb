01595764c0799f95cc58a836e605e2e71c96a669
yudaiishiyama <yude3939@gmail.com>
2022-05-25 14:22:36 +0900

    修正１

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -102,6 +102,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
+      it '価格に半角数字以外が含まれている場合は出品できない' do
+        @item.price = "１１１"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
     end
   end
 end
