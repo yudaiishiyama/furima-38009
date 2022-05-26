e1bfb2549e50742922cf83ae6b7563ee72151015
yudaiishiyama <yude3939@gmail.com>
2022-05-26 21:13:47 +0900

    rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-  
+
   def edit
     @item = Item.find(params[:id])
     if @item.user.id == current_user.id
